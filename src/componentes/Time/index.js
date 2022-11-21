344a8c78d9607e45630cf44bed7f080e89cac866
Melissa Ost de Vasconcelos <melissa.ost.gmi@mprj.mp.br>
2022-11-21 11:27:25 -0300

    inserindo colaborador no time

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,11 +1,18 @@
+import Colaborador from '../Colaborador'
 import './Time.css'
 
 const Time = (props) => {
-    const corSec = {backgroundColor : props.corSecundaria}
-    const borda = {borderColor: props.corPrimaria}
-    return(
-        <section className='time' style={corSec}>
+    const corSec = { backgroundColor: props.corSecundaria }
+    const borda = { borderColor: props.corPrimaria }
+    return (
+        (props.colaboradores.length > 0 ) && <section className='time' style={corSec}>
             <h3 style={borda}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map(colaborador =>
+                    <Colaborador nome={colaborador.nome}
+                        cargo={colaborador.cargo}
+                        imagem={colaborador.imagem} />)}
+            </div>
         </section>
     )
 }
